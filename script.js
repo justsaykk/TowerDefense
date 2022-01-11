0e6b792cb1ef26d8f0274bced2a2a5fa620b50f3
“justsaykk <kaikein.woo@gmail.com>
2022-01-11 19:30:32 +0800

    Added exisiting defender check

--- script.js
+++ script.js
@@ -3,10 +3,31 @@ const ctx = canvas.getContext('2d');
 canvas.width = 900;
 canvas.height = 600;
 
-/* Global Variables */
+// Global Variables
 const cellSize = 100;
 const cellGap = 3;
 const gameGrid = [];
+const defenders = [];
+let defenderCost = 100;
+let numberOfResources = 500;
+
+// mouse
+const mouse = {
+    x: undefined,
+    y: undefined,
+    width: 0.1,
+    height: 0.1,
+}
+
+let canvasPosition = canvas.getBoundingClientRect();
+canvas.addEventListener('mousemove', e => {
+    mouse.x = e.x - canvasPosition.left;
+    mouse.y = e.y - canvasPosition.top;
+});
+canvas.addEventListener('mouseleave', () => {
+    mouse.x = undefined;
+    mouse.y = undefined;
+})
 
 //game board
 const controlsBar = {
@@ -44,32 +65,62 @@ const handleGameGrid = () => {
     }
 }
 
-// mouse
-const mouse = {
-    x: undefined,
-    y: undefined,
-    width: 0.1,
-    height: 0.1,
-}
-
-let canvasPosition = canvas.getBoundingClientRect();
-canvas.addEventListener('mousemove', e => {
-    mouse.x = e.x - canvasPosition.left;
-    mouse.y = e.y - canvasPosition.top;
-});
-canvas.addEventListener('mouseleave', () => {
-    mouse.x = undefined;
-    mouse.y = undefined;
-})
-
-
-
-
 // projectiles
 // defenders
+class Defender {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.width = cellSize;
+        this.height = cellSize;
+        this.shoot = false;
+        this.health = 100;
+        this.projectiles = [];
+        this.timer = 0;
+    }
+
+    draw(){
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = 'gold';
+        ctx.font = '20px Arial';
+        ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 25);
+    }
+}
+
+canvas.addEventListener('click', e => {
+    const gridPositionX = mouse.x - (mouse.x % cellSize);
+    const gridPositionY = mouse.y - (mouse.y % cellSize);
+    if (gridPositionY < cellSize) { // Error handler if user clicks on HUD
+        return;
+    }
+    for (let i = 0; i < defenders.length; i++){ // Check that clicked spot do not have an existing defender
+        if (defenders[i].x === gridPositionX && defenders[i].y === gridPositionY) {
+            return;
+        }
+    }
+    let defenderCost = 100;
+    if (numberOfResources >= defenderCost) {
+        defenders.push(new Defender(gridPositionX, gridPositionY))
+        numberOfResources -= defenderCost;
+    }
+})
+
+const handleDefenders = () => {
+    for (let i = 0; i < defenders.length; i++) {
+        defenders[i].draw();
+    }
+}
+
+
 // enemies
-//resources
-/* Utilities */
+// resources
+// Utilities
+const handleGameStatus = () => {
+    ctx.fillStyle = 'gold';
+    ctx.font = '30px Arial';
+    ctx.fillText("Resources: " + numberOfResources, 20, 55);
+}
 
 // Collision Function //
 const isCollision = (first, second) => {
@@ -89,6 +140,8 @@ const animate = () => {
     ctx.fillStyle = 'blue';
     ctx.fillRect(0, 0,controlsBar.width,controlsBar.height);
     handleGameGrid();
+    handleDefenders();
+    handleGameStatus();
     requestAnimationFrame(animate);
 }
 animate(); 
